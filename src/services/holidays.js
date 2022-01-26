de38af54515488b552dbbf8948920de49ef47b53
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-25 23:54:58 -0800

    filtering by user search working correctly

--- src/services/holidays.js
+++ src/services/holidays.js
@@ -1,6 +1,6 @@
 export const fetchHolidays = async () => {
   const resp = await fetch(
-    'https://date.nager.at/api/v3/NextPublicHolidaysWorldwide'
+    'https://date.nager.at/api/v3/PublicHolidays/2022/US'
   );
   const data = await resp.json();
 
