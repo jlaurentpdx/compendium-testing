617ee3885634d6dadad8141cfa901bebe79f2745
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-25 21:54:19 -0800

    data from fetch call being stored in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchHolidays } from './services/holidays';
 
 export default function App() {
-  useEffect(async () => {
-    console.log(await fetchHolidays());
+  const [loading, setLoading] = useState(true);
+  const [holidays, setHolidays] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchHolidays();
+      setHolidays(data);
+      setLoading(false);
+    };
+    fetchData();
   }, []);
+
+  console.log(holidays);
 
   return <h1>Hello World</h1>;
 }
