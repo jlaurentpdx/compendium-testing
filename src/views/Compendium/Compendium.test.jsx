f5154053424a5463861b6c4c1f8726d5daa75c25
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-27 17:01:23 -0800

    added test for search input to appear

--- src/views/Compendium/Compendium.test.jsx
+++ src/views/Compendium/Compendium.test.jsx
@@ -1,14 +1,20 @@
 import {
   render,
   screen,
-  waitFor,
   waitForElementToBeRemoved,
 } from '@testing-library/react';
 import Compendium from './Compendium';
 
-test('our compendium renders an initial loading state', async () => {
+test('Compendium renders an initial loading state', async () => {
   render(<Compendium />);
 
   expect(screen.getByText(/please/i)).toBeInTheDocument();
   await waitForElementToBeRemoved(() => screen.getByText(/please/i));
+});
+
+test('Compendium renders a search bar', async () => {
+  render(<Compendium />);
+
+  const search = await screen.findByRole('textbox');
+  expect(search).toBeInTheDocument();
 });
